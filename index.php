856b9d9aae0df3635beb9c1d470c65546ff1c292
James <j.benson13@hotmail.co.uk>
2018-09-20 13:17:13 +0100

    Started writing failure test

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <body>
 <?php
 
-$suit = ['A' => 11, 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7, 8 => 8, 9 => 9, 10 => 10, 'J' => 10, 'Q' => 10, 'K' => 10];
+$suit = ['A' => 11, '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9, '10' => 10, 'J' => 10, 'Q' => 10, 'K' => 10];
 $deck = ['hearts' => $suit, 'diams' => $suit, 'clubs' => $suit, 'spades' => $suit];
 
 /*
@@ -17,7 +17,7 @@ $deck = ['hearts' => $suit, 'diams' => $suit, 'clubs' => $suit, 'spades' => $sui
  */
 function pickOneCard(array $deck) : array {
     $suit = array_rand($deck);
-    $card = array_rand($deck[$suit]);
+    $card = (string)array_rand($deck[$suit]);
     unset($deck[$suit][$card]);
     return [$card, $suit, $deck];
 }
--- test/index.php
+++ test/index.php
@@ -1,2 +1,24 @@
 <?php
+
 require('../index.php');
+
+use PHPUnit\Framework\TestCase;
+
+
+class StackTest extends TestCase {
+    const SUIT = ['A' => 11, '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9, '10' => 10, 'J' => 10, 'Q' => 10, 'K' => 10];
+    const DECK = ['hearts' => self::SUIT, 'diams' => self::SUIT, 'clubs' => self::SUIT, 'spades' => self::SUIT];
+
+    public function testpickOneCard_success() {
+        $card = pickOneCard(self::DECK);
+        $this->assertCount(3, $card);
+        $this->assertInternalType('string', $card[0]);
+        $this->assertInternalType('string', $card[1]);
+        $this->assertInternalType('array', $card[2]);
+    }
+
+    public function testpickOneCard_failure() {
+        $card = pickOneCard([]);
+        $this->expectException(InvalidArgumentException::class);
+    }
+}
